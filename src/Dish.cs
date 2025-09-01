cae554fcbcad0b6a2f9b803fc6b369841fb0721a
MiliGarciaDaSilva <mariamilagrosgarciadasilva@gmail.com>
2025-09-01 16:30:43 -0300

    Implemento clase Dish

--- src/Dish.cs
+++ src/Dish.cs
@@ -6,5 +6,16 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Dish
     {
+        private string name;
+        private double price;
+        private bool isVegetarian;
+
+        public Dish(string name, double price, bool isVegetarian)
+        {
+            this.name = name;
+            this.price = price;
+            this.isVegetarian = isVegetarian;
+        }
+        
     }
 }
