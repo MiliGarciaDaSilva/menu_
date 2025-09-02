b328d8bbe685da2ef63936975509ae0f8ab24e51
MiliGarciaDaSilva <mariamilagrosgarciadasilva@gmail.com>
2025-09-02 01:03:07 -0300

    agrego clase Table

--- src/Table.cs
+++ src/Table.cs
@@ -7,8 +7,33 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Table
     {
+        public int Number  { get; }
+        public bool IsOccupied { get; set; }
         private List<Dish> order = new List<Dish>();
 
+        public Table(int number, bool isOccupied)
+        {
+            Number = number;
+            IsOccupied = isOccupied;
+            order = new List<Dish>();
+        }
+
+        public void Occupy()
+        {
+            IsOccupied = true;
+        }
+
+        public void Free()
+        {
+            IsOccupied = false;
+            order.Clear();
+        }
+
+        public void AddOrder(Dish dish)
+        {
+            order.Add(dish);
+        }
+        
         public bool HasOrders()
         {
             return this.order.Count > 0;
